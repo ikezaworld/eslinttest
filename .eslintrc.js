94949764ca348e1e09ca19b1fc1621981a264cfd
ikezaworld <63359463+ikezaworld@users.noreply.github.com>
2020-04-16 16:24:38 +0900

    add rc file

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "prettier",
+    ],
+    "plugins": [
+        "prettier"
+    ],
+    "rules": {
+        "prettier/prettier": "warn"
+        ,"prefer-template": "warn"
+    }
+};
